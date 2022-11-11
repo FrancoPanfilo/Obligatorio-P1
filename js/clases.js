e41b05902616b9a7d17312572492ccd2e03fba76
FrancoPanfilo <108556532+FrancoPanfilo@users.noreply.github.com>
2022-11-11 18:44:50 -0300

    paco

--- js/clases.js
+++ js/clases.js
@@ -1,54 +1,61 @@
 //Sebastian Garcia 281637 y Alfonso Lopez 293449
-class Presentacion{
-    constructor(empresa, titulo, descripcion, tema, dia, duracion){
-        this.empresa= empresa;
-        this.titulo= titulo;
-        this.descripcion= descripcion;
-        this.tema= tema;
-        this.dia= dia;
-        this.duracion= duracion;
-    }
+class Presentacion {
+  constructor(empresa, titulo, descripcion, tema, dia, duracion) {
+    this.empresa = empresa;
+    this.titulo = titulo;
+    this.descripcion = descripcion;
+    this.tema = tema;
+    this.dia = dia;
+    this.duracion = duracion;
+  }
 }
 
-class Empresa{
-    constructor(nombre, direccion, telefono, origen){
-        this.nombre= nombre;
-        this.direccion= direccion;
-        this.telefono= telefono;
-        this.origen= origen;
-    }
-    toString(){
-
-    }
+class Empresa {
+  constructor(nombre, direccion, telefono, origen) {
+    this.nombre = nombre;
+    this.direccion = direccion;
+    this.telefono = telefono;
+    this.origen = origen;
+  }
+  toString() {}
 }
-class Sistema{
-    constructor(){
-        this.listaEmpresa=[];
-        this.listaPresentaciones=[];
+class Sistema {
+  constructor() {
+    this.listaEmpresa = [];
+    this.listaPresentaciones = [];
+  }
+  empresasMaxPresentacion() {
+    let listaFinal = [];
+    let maximo = 0;
+    for (let empresa of this.listaEmpresa) {
+      let nuevaCantidad = 0;
+      for (let oferta of this.listaEmpresa) {
+        if (Empresa.nombre == empresa) {
+          //INCOMPLETO, CAMBIAR NOMBRES
+          nuevaCantidad++;
+          if (nuevaCantidad > maximo) {
+            maximo = nuevaCantidad;
+            listaFinal = [empresa];
+          } else {
+            if (sumaOfer == maximo) {
+              listaFinal.push(empresa);
+            }
+          }
+          console.log(maximo);
+        }
+      }
     }
-    empresasMaxPresentacion(){
-		let listaFinal = [];
-		let maximo = 0;
-		for(let empresa of this.listaEmpresa){
-			let nuevaCantidad = 0;
-			for(let oferta of this.listaEmpresa){
-				if(Empresa.nombre == empresa){                                   //INCOMPLETO, CAMBIAR NOMBRES
-					nuevaCantidad++;
-					if(nuevaCantidad > maximo){
-						maximo = nuevaCantidad;
-						listaFinal = [empresa];
-					}
-					else{
-						if(sumaOfer == maximo){
-							listaFinal.push(empresa);
-						}
-					}
-					console.log(maximo);
-				}
-			}
-		}
-		return listaFinal;
-	}
-
+    return listaFinal;
+  }
+  mostarENLista(empresa, sistema) {
+    let noEstaEnLista = true;
+    for (let i = 0; i < sistema.listaEmpresa.length; i++) {
+      if (sistema.listaEmpresa[i].nombre == empresa.nombre) {
+        noEstaEnLista = false;
+      }
+    }
+    if (empresa.nombre == "" && noEstaEnLista) {
+      sistema.listaEmpresa.push(empresa);
+    }
+  }
 }
-
--- js/funciones.js
+++ js/funciones.js
@@ -1,94 +1,117 @@
 //Sebastian Garcia 281637 y Alfonso Lopez 293449
-window.addEventListener("load",inicio);
+window.addEventListener("load", inicio);
 
-var contadorTipo1=0;
-var contadorTipo2=0;
-var contadorTipo3=0;
-var contadorTipo4=0;
-var contadorTipo5=0;
-var contadorTipo6=0;
+var contadorTipo1 = 0;
+var contadorTipo2 = 0;
+var contadorTipo3 = 0;
+var contadorTipo4 = 0;
+var contadorTipo5 = 0;
+var contadorTipo6 = 0;
 
-let sistema= new Sistema();
+let sistema = new Sistema();
 
-function inicio(){
-    document.getElementById("BotonAgregar").addEventListener("click",proceso);
-    document.getElementById("empresaAltadePresentacion").addEventListener("change",seleccionCombo);
-    document.getElementById("BotonAgregar2").addEventListener("click",proceso2);
+function inicio() {
+  document.getElementById("BotonAgregar").addEventListener("click", proceso);
+  document
+    .getElementById("empresaAltadePresentacion")
+    .addEventListener("change", seleccionCombo);
+  document.getElementById("BotonAgregar2").addEventListener("click", proceso2);
 }
-function seleccionCombo(){
-    let elegido=document.getElementById("empresaAltadePresentacion").value;
-    alert("eligio "+ elegido);
+function seleccionCombo() {
+  let elegido = document.getElementById("empresaAltadePresentacion").value;
+  alert("eligio " + elegido);
 }
-function proceso(){
-    let formulario= document.getElementById("formularioAltadeEmpresa");
-    if(formulario.reportValidity()){
-            for (let i=0;i<sistema.listaEmpresa.length;i++){
-                if(document.getElementById("nombreAltadeEmpresa").value==sistema.listaEmpresa[i]){
-                    alert("Esta empresa ya fue registrada");
-                }
-                //else{
-            }
-                    let nombre= document.getElementById("nombreAltadeEmpresa").value;
-                    let direccion= document.getElementById("direccionAltadeEmpresa").value;
-                    let telefono= document.getElementById("telefonoAltadeEmpresa").value;
-                    let origenNacional= document.getElementById("nacionalAltas");
-                    let origenExtranjera= document.getElementById("extranjeraAltas");
-                    cargarCombo(nombre);
-                    formulario.reset();   
-                //}
-            //}
+function proceso() {
+  let formulario = document.getElementById("formularioAltadeEmpresa");
+  if (formulario.reportValidity()) {
+    let nombre = document.getElementById("nombreAltadeEmpresa").value;
+    let direccion = document.getElementById("direccionAltadeEmpresa").value;
+    let telefono = document.getElementById("telefonoAltadeEmpresa").value;
+    let noEstaEnLista = true;
+    for (let i = 0; i < sistema.listaEmpresa.length; i++) {
+      if (nombre == sistema.listaEmpresa[i].nombre) {
+        alert("Esta empresa ya fue registrada");
+        noEstaEnLista = false;
+      }
     }
-}
-function cargarCombo(texto){
-    document.getElementById("empresaAltadePresentacion").innerHTML = "";                                        
-    let combo=document.getElementById("empresaAltadePresentacion");
-    sistema.listaEmpresa.push(document.getElementById("nombreAltadeEmpresa").value);
-    for (let i=0; i<sistema.listaEmpresa.length;i++){
-        let nodo=document.createElement("option");                          //creo una nueva option
-        let nodoTexto=document.createTextNode(sistema.listaEmpresa[i]);          //creo lo que va a tener escrito la opcion (nodo de texto)
-        nodo.appendChild(nodoTexto);                                        //vinculo la nueva opcion al nuevo texto
-        combo.appendChild(nodo);                                            //vinculo la nueva opcion ya con su texto correspondiente al select 
+    let vacios = nombre == "" || direccion == "" || telefono == "";
+    if (vacios) {
+      alert("Faltan datos");
     }
+    if (noEstaEnLista && !vacios) {
+      cargarCombo(nombre);
+      formulario.reset();
+    }
+  }
+}
+function cargarCombo(texto) {
+  document.getElementById("empresaAltadePresentacion").innerHTML = "";
+  let combo = document.getElementById("empresaAltadePresentacion");
+  let nombre = document.getElementById("nombreAltadeEmpresa").value;
+  let direccion = document.getElementById("direccionAltadeEmpresa").value;
+  let telefono = document.getElementById("telefonoAltadeEmpresa").value;
+  let origenExtranjera = document.getElementById("extranjeraAltas");
+  let origen;
+  if (origenExtranjera.checked) {
+    origen = "Extranjera";
+  } else {
+    origen = "Nacional";
+  }
+  let empresa = new Empresa(nombre, direccion, telefono, origen);
+  sistema.listaEmpresa.push(empresa);
+  agregarElementoEnLista(empresa);
+  for (let i = 0; i < sistema.listaEmpresa.length; i++) {
+    let nodo = document.createElement("option"); //creo una nueva option
+    let nodoTexto = document.createTextNode(sistema.listaEmpresa[i].nombre); //creo lo que va a tener escrito la opcion (nodo de texto)
+    nodo.appendChild(nodoTexto); //vinculo la nueva opcion al nuevo texto
+    combo.appendChild(nodo); //vinculo la nueva opcion ya con su texto correspondiente al select
+  }
 }
 
-function proceso2(){
-    let formulario=document.getElementById("formularioAltadePresentacion");
-    if(formulario.reportValidity()){
-        let empresa= document.getElementById("empresaAltadePresentacion").value;
-        let titulo= document.getElementById("tituloAltadePresentacion").value;
-        let descripcion= document.getElementById("descripcionAltadePresentacion").value;
-        let tema= document.getElementById("temaAltadePresentacion");
-        let dia= document.getElementById("extranjeraAltas");
-        let duracion= document.getElementById("duracionAltadePresentacion");
-        agregarElementoEnLista(empresa);
-            if (tema.value=="inteligenciaArtificial"){
-                contadorTipo1++;
-            }     
+function proceso2() {
+  let formulario = document.getElementById("formularioAltadePresentacion");
+  if (formulario.reportValidity()) {
+    let empresa = document.getElementById("empresaAltadePresentacion").value;
+    let titulo = document.getElementById("tituloAltadePresentacion").value;
+    let descripcion = document.getElementById(
+      "descripcionAltadePresentacion"
+    ).value;
+    let tema = document.getElementById("temaAltadePresentacion");
+    let dia = document.getElementById("extranjeraAltas");
+    let duracion = document.getElementById("duracionAltadePresentacion");
+    if (tema.value == "inteligenciaArtificial") {
+      contadorTipo1++;
     }
+  }
 }
-function agregarElementoEnLista(texto){
-    // creo un elemento de lista
+function agregarElementoEnLista() {
+  let empresas = sistema.listaEmpresa;
+  // obtengo la lista y la vacio
+  let lista = document.getElementById("listaEmpresasRegistradas");
+  lista.innerHTML = "";
+  // lleno la lista con el array
+  for (let i = 0; i < empresas.length; i++) {
     let node = document.createElement("LI");
     // creo nodo de texto, dentro lleva el texto que quiero
-    let  textnode = document.createTextNode(texto);
+    let textnode = document.createTextNode(
+      `${empresas[i].nombre} , ${empresas[i].direccion}, ${empresas[i].telefono} y es de origen ${empresas[i].origen}`
+    );
     // engancho al nodo de lista el nodo de texto
     node.appendChild(textnode);
-    // engancho el nodo a la lista 
-    document.getElementById("listaEmpresasRegistradas").appendChild(node);
+    // engancho el nodo a la lista
+
+    lista.appendChild(node);
+  }
 }
-function cargarTabla(){
+function cargarTabla() {}
 
-}
-
-
-function cargarListaMaxPresentaciones(){
-    document.getElementById("listaMaxPresentaciones").innerHTML = "";
-    let empresasMax= sistema.empresasMaxPresentacion();
-    if(empresasMax>0){
-        //for(){
-
-        //}
-    }
+function cargarListaMaxPresentaciones() {
+  document.getElementById("listaMaxPresentaciones").innerHTML = "";
+  let empresasMax = sistema.empresasMaxPresentacion();
+  if (empresasMax > 0) {
+    //for(){
+    //}
+  }
 }
 
-empresasMax=[]
+empresasMax = [];
